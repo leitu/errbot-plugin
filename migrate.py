92638ba175963d93994137194978ff9535641c29
Alex Tu <alex.lei.tu@gmail.com>
2016-01-22 16:04:21 +0800

    local change

--- migrate.py
+++ migrate.py
@@ -1,17 +1,36 @@
 from errbot import BotPlugin, botcmd
 from errbot.templating import tenv
 import subprocess, os.path
-import paramiko
+import paramiko,time
 
 class Migrate(BotPlugin):
     """Use paramiko to connect remote server"""
-    def remote_excute(self, script, remote_server='10.10.0.3', username="root", password="_aoO1CBaYbshr1VS_LGh"):
+    def remote_excute(self, script):
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(remote_server, username, password)
-        stdin, stdout, stderr = client.exec_command(script)
-        res = stdout.channel.recv_exit_status()
-        stdin.close()
+        client.connect('10.10.0.3', username="root", password="_aoO1CBaYbshr1VS_LGh",timeout=60)
+        #client.connect('10.6.15.109', username='root', password='58mfJGsv#q@4')
+        sleeptime = 0.001
+        outdata, errdata = '', ''
+        ssh_transp = client.get_transport()
+        chan = ssh_transp.open_session()
+        # chan.settimeout(3 * 60 * 60)
+        chan.setblocking(0)
+        chan.exec_command(script)
+        while True:  # monitoring process
+            # Reading from output streams
+            while chan.recv_ready():
+                outdata += chan.recv(1000)
+            while chan.recv_stderr_ready():
+                errdata += chan.recv_stderr(1000)
+            if chan.exit_status_ready():  # If completed
+                break
+            time.sleep(sleeptime)
+        res = chan.recv_exit_status()
+        ssh_transp.close()
+
+        #print(outdata)
+        #print(errdata)
         return res
       
     @botcmd(split_args_with=None, template="migrate")
@@ -22,7 +41,9 @@ class Migrate(BotPlugin):
         numbers = args.pop(0)
         nfs = args.pop(0)
 
-        script =  "/root/stephen/storage_migrations/migrate-env-atu.sh" + " " + user + " " + environment + " " + numbers + " " + nfs
+        script =  "/root/stephen/storage_migrations/migrate-env-atu.sh" + " -u " + user + " -e " + environment + " -n " + numbers + " -s " + nfs
+        #script = "/opt/chef/embedded/bin/ruby /usr/bin/chef-client -d -c /etc/chef/client.rb -L /var/log/chef/client.log -P /var/run/chef/client.pid -i 3600 -s 600 --fork"
+
 
         res = self.remote_excute(script)
         if res == 0:
@@ -39,7 +60,8 @@ class Migrate(BotPlugin):
         dc = args.pop(0)
         datastore = args.pop(0)
  
-        script = '/root/stephen/storage_migrations/start-env-atu.sh' + " " + environment + " " + numbers + " " + dc + " " + datastore
+        script = '/root/stephen/storage_migrations/start-env-atu.sh' + " -e " + environment + " -n " + numbers + " -d " + dc + " -s " + datastore
+        #script = "/opt/chef/embedded/bin/ruby /usr/bin/chef-client -d -c /etc/chef/client.rb -L /var/log/chef/client.log -P /var/run/chef/client.pid -i 3600 -s 600 --fork"
         res = self.remote_excute(script)
         if res == 0:
             response = tenv().get_template('start.md').render(environment=environment)
